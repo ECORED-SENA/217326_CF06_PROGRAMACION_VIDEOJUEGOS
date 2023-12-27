623849ec1319413efbb6718a2917db0533eeb9d8
elrilacho <camilobr17@gmail.com>
2023-12-27 16:13:21 -0500

    pdf_zip

--- src/config/global.js
+++ src/config/global.js
@@ -193,7 +193,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF06_ 217326_DU.pdf',
       },
       {
         icono: 'fas fa-download',
